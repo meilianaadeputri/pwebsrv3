04f9cf1392cd58f914f3cc91552a277a9ac4a194
meilianaadeputri <meilianaadeputri19@rharismac.id>
2021-02-13 19:23:31 +0800

    testos

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -17,14 +17,12 @@
         <li><a href="rooms">Rooms</a></li>
         <li><a href="guests">Guests</a></li>
         <li><a href="members">Members</a></li>
-        <li><a href="login">Login</a></li>
       </ul>
 
       <ul id="nav-mobile" class="sidenav">
         <li><a href="rooms">Rooms</a></li>
         <li><a href="guests">Guests</a></li>
         <li><a href="members">Members</a></li>
-        <li><a href="login">Login</a></li>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
